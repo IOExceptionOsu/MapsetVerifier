49d0b1f5f2674e0cdc31b0ea2fda9c2fc6fd9d04
IOException <ioexceptionosu@gmail.com>
2020-09-12 03:27:32 -0500

    add all the checks back in + hook up the left tab pane

--- MapsetVerifier/Form1.cs
+++ MapsetVerifier/Form1.cs
@@ -1,17 +1,22 @@
 ﻿using MapsetChecks.checks.compose;
+using MapsetChecks.checks.events;
+using MapsetChecks.checks.general.audio;
+using MapsetChecks.checks.general.files;
+using MapsetChecks.checks.general.metadata;
+using MapsetChecks.checks.general.resources;
 using MapsetChecks.checks.hit_sounds;
+using MapsetChecks.checks.settings;
+using MapsetChecks.checks.spread;
+using MapsetChecks.checks.standard.compose;
+using MapsetChecks.checks.standard.settings;
+using MapsetChecks.checks.standard.spread;
 using MapsetChecks.checks.timing;
 using MapsetVerifierFramework;
+using MapsetVerifierFramework.objects;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Diagnostics;
-using System.Drawing;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace MapsetVerifier
@@ -26,16 +31,103 @@ namespace MapsetVerifier
 
         private void addAllChecks()
         {
-            CheckerRegistry.RegisterCheck(new CheckAbnormalNodes());
-            CheckerRegistry.RegisterCheck(new CheckHitSounds());
-            CheckerRegistry.RegisterCheck(new CheckWrongSnapping());
+            new List<Check>()
+            {
+                // all modes -> compose
+                new CheckAbnormalNodes(),
+                new CheckConcurrent(),
+                new CheckDrainTime(),
+                new CheckInvisibleSlider(),
+
+                // all modes -> events
+                new CheckBreaks(),
+                new CheckStoryHitSounds(),
+
+                // all modes -> general -> audio
+                // new CheckAudioFormat(),
+                new CheckAudioInVideo(),
+                new CheckAudioUsage(),
+                // new CheckBitrate(),
+                new CheckCommonFinish(),
+                new CheckHitSoundDelay(),
+                // new CheckHitSoundFormat(),
+                new CheckHitSoundImbalance(),
+                new CheckHitSoundLength(),
+                new CheckMultipleAudio(),
+
+                // all modes -> general -> files
+                new CheckUnusedFiles(),
+                new CheckUpdateValidity(),
+                new CheckZeroBytes(),
+
+                // all modes -> general -> metadata
+                new CheckGenreLanguage(),
+                new CheckGuestTags(),
+                new CheckInconsistentMetadata(),
+                new CheckMarkerFormat(),
+                new CheckMarkerSpacing(),
+                new CheckSource(),
+                new CheckTitleMarkers(),
+                new CheckUnicode(),
+
+                // all modes -> general -> resources
+                new CheckBgPresence(),
+                new CheckBgResolution(),
+                new CheckMultipleVideo(),
+                new CheckOverlayLayer(),
+                new CheckSpriteResolution(),
+                new CheckVideoOffset(),
+                new CheckVideoResolution(),
+
+                // all modes -> hit sounds
+                new CheckHitSounds(),
+                new CheckMuted(),
+
+                // all modes -> settings
+                new CheckDiffSettings(),
+                new CheckInconsistentSettings(),
+                new CheckTickRate(),
+
+                // all modes -> spread
+                new CheckLowestDiff(),
+
+                // all modes -> timing
+                new CheckBeforeLine(),
+                new CheckConcurrentLines(),
+                new CheckFirstLine(),
+                new CheckInconsistentLines(),
+                new CheckKiaiUnsnap(),
+                new CheckPreview(),
+                new CheckUnsnaps(),
+                new CheckUnusedLines(),
+                new CheckWrongSnapping(),
+
+                // standard -> compose
+                new CheckAmbiguity(),
+                new CheckBurai(),
+                new CheckNinjaSpinner(),
+                new CheckObscuredReverse(),
+                new CheckOffscreen(),
+
+                // standard -> settings
+                new CheckDefaultColours(),
+                new CheckLuminosity(),
+
+                // standard -> spread
+                new CheckCloseOverlap(),
+                new CheckMultipleReverses(),
+                new CheckShortSliders(),
+                new CheckSpaceVariation(),
+                new CheckSpinnerRecovery(),
+                new CheckStackLeniency(),
+            }.ForEach(CheckerRegistry.RegisterCheck);
         }
 
         private void launchOpenDialog(object sender, ToolStripItemClickedEventArgs e)
         {
             var dialog = new FolderBrowserDialog();
             dialog.RootFolder = Environment.SpecialFolder.Desktop;
-            dialog.SelectedPath= Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\osu!\Songs";
+            dialog.SelectedPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\osu!\Songs";
             dialog.ShowDialog();
 
             if (Directory.Exists(dialog.SelectedPath))
--- MapsetVerifier/MapsetTab.cs
+++ MapsetVerifier/MapsetTab.cs
@@ -15,16 +15,23 @@ namespace MapsetVerifier
     internal class MapsetTab : TabPage
     {
         private BeatmapSet set;
+        private string setPath;
+        private ulong setId;
+        private Dictionary<string, List<Issue>> lastRunIssues = null;
+        private string currentlySelectedTab = null;
 
         private TableLayoutPanel panel;
         private ListBox diffList;
         private ToolStrip toolbar;
-        private ToolStripButton openMapsetInBrowserBtn;
+        private ToolStripButton openMapsetInExplorerBtn, openMapsetInBrowserBtn;
         private RichTextBox tb;
 
         public MapsetTab(string path)
         {
+            setPath = path;
             set = new BeatmapSet(path);
+            var map = set.beatmaps.Find(bmap => bmap.metadataSettings.beatmapId > 0 && bmap.metadataSettings.beatmapSetId > 0);
+            setId = (ulong) map.metadataSettings.beatmapSetId;
 
             #region setting up controls
             panel = new TableLayoutPanel();
@@ -33,22 +40,30 @@ namespace MapsetVerifier
             diffList = new ListBox();
             diffList.Dock = DockStyle.Left;
             diffList.Items.Add("General");
+            diffList.SelectedIndexChanged += changeSelectedDifficulty;
             foreach (var diff in set.beatmaps) {
-                diffList.Items.Add(diff.metadataSettings.version);
+                diffList.Items.Add($"[{diff.metadataSettings.version}]");
             }
             panel.Controls.Add(diffList, 0, 1);
 
             toolbar = new ToolStrip();
+            openMapsetInExplorerBtn = new ToolStripButton();
+            openMapsetInExplorerBtn.Text = "Open in Explorer";
+            openMapsetInExplorerBtn.DisplayStyle = ToolStripItemDisplayStyle.Text;
+            openMapsetInExplorerBtn.Click += openMapsetInExplorer;
+            toolbar.Items.Add(openMapsetInExplorerBtn);
+
             openMapsetInBrowserBtn = new ToolStripButton();
             openMapsetInBrowserBtn.Text = "Open in Browser";
-            openMapsetInBrowserBtn.DisplayStyle = ToolStripItemDisplayStyle.ImageAndText;
-            openMapsetInBrowserBtn.Image = SystemIcons.Information.ToBitmap();
-            openMapsetInBrowserBtn.Enabled = false;
-            toolbar.Items.Add("Open In Browser");
+            openMapsetInBrowserBtn.DisplayStyle = ToolStripItemDisplayStyle.Text;
+            openMapsetInBrowserBtn.Enabled = setId > 0;
+            openMapsetInBrowserBtn.Click += openMapsetInBrowser;
+            toolbar.Items.Add(openMapsetInBrowserBtn);
             panel.Controls.Add(toolbar, 0, 0);
             panel.SetColumnSpan(toolbar, 2);
 
             tb = new RichTextBox();
+            tb.Text = "loading...";
             tb.Multiline = true;
             tb.LinkClicked += handleOpenLink;
             tb.Dock = DockStyle.Fill;
@@ -59,6 +74,36 @@ namespace MapsetVerifier
 
             // start running checks
             _ = Task.Run(runChecks);
+        }
+
+        private void openMapsetInExplorer(object sender, System.EventArgs e)
+        {
+            Process.Start("explorer.exe", setPath);
+        }
+
+        private void openMapsetInBrowser(object sender, System.EventArgs e)
+        {
+            var uri = $"https://osu.ppy.sh/beatmapsets/{setId}";
+            Trace.WriteLine("opening " + uri);
+            var psi = new ProcessStartInfo();
+            psi.UseShellExecute = true;
+            psi.FileName = uri;
+            Process.Start(psi);
+        }
+
+        private void changeSelectedDifficulty(object sender, System.EventArgs e)
+        {
+            if (lastRunIssues == null) return;
+            if (diffList.SelectedItem == null) return;
+            changeTabTo((string)diffList.SelectedItem);
+        }
+
+        private void changeTabTo(string tab)
+        {
+            List<Issue> issues;
+            var found = lastRunIssues.TryGetValue(tab, out issues);
+            if (found)
+                tb.Rtf = constructRtf(issues);
         }
 
         private void handleOpenLink(object sender, LinkClickedEventArgs e)
@@ -84,19 +129,30 @@ namespace MapsetVerifier
         private void finishRunningChecks(List<Issue> issues)
         {
             tb.Clear();
-            tb.Rtf = constructRtf(issues);
-            /* tb.Rtf = @"{\rtf1\ansi\deff0 {\fonttbl {\f0 Arial;}}
-{\field{\*\fldinst HYPERLINK ""URL""}{\fldrslt{\ul\cf1Text to display}}}
-}"; */
+            lastRunIssues = new Dictionary<string, List<Issue>>();
+            foreach (var issue in issues)
+            {
+                var tabName = "General";
+                if (issue.beatmap != null) 
+                    tabName = $"[{issue.beatmap.metadataSettings.version}]";
+                if (!lastRunIssues.ContainsKey(tabName))
+                {
+                    lastRunIssues.Add(tabName, new List<Issue>());
+                }
+                lastRunIssues[tabName].Add(issue);
+            }
+         
+            if (currentlySelectedTab != null)
+            {
+                changeTabTo(currentlySelectedTab);
+            }
         }
-
-        // 01:38:518 (1) - 
-        // 01:38:518 (1,2) - 
+        
         private static Regex editorLinks = new Regex(
             @"(osu:\/\/edit\/)?(?<entire>(?<timestamp>\d+:\d{2}:\d{3})( \([^\)]*\))?( -)?)",
             RegexOptions.Compiled | RegexOptions.ExplicitCapture
         );
-        private string constructRtf(List<Issue> issues)
+        private string constructRtf(IEnumerable<Issue> issues)
         {
             var doc = new StringBuilder();
             doc.AppendLine(@"{\rtf1\ansi\deff0 {\fonttbl {\f0 Arial;}}");
@@ -106,7 +162,7 @@ namespace MapsetVerifier
                 Trace.WriteLine(matches.Count + " " + matches.ToString());
                 var text = editorLinks.Replace(issue.message, match => 
                     $@"{{\field{{\*\fldinst HYPERLINK ""osu://edit/{match.Groups["entire"]}""}}{{\fldrslt {match.Groups["entire"]}}}}}");
-                doc.AppendLine(text);
+                doc.AppendLine($@"{{\pard\sa200 - {text} \par}}");
             }
             doc.AppendLine(@"}");
             return doc.ToString();
